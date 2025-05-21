8c86afc458eb3ad45127832bfcb43bc994393828
marcodistefano90 <distefanomarco90@gmail.com>
2025-05-21 18:44:47 -0400

    definitiva

--- src/models/order_item.py
+++ src/models/order_item.py
@@ -1,10 +1,10 @@
 from src.extensions import db
 
-class OrderItem(db.Model): tablename = 'order_items'
+class OrderItem(db.Model):
+    __tablename__ = 'order_items'
 
-order_id = db.Column(db.String(36), db.ForeignKey('orders.id'), primary_key=True)
-product_id = db.Column(db.String(36), db.ForeignKey('products.id'), primary_key=True)
-quantity = db.Column(db.Integer, nullable=False)
+    order_id = db.Column(db.String(36), db.ForeignKey('orders.id'), primary_key=True)
+    product_id = db.Column(db.String(36), db.ForeignKey('products.id'), primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
 
-product = db.relationship('Product', backref='order_items')
-
+    product = db.relationship('Product', backref='order_items')
